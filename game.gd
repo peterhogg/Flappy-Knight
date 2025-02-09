513ff8521f7791d590cdd61eb43e7d1a70e5d0dd
Peter Hoggarth <hoggarth.peter@gmail.com>
2025-02-09 10:10:22 -0500

    Restarts game on obstacle collision

--- game.gd
+++ game.gd
@@ -5,6 +5,7 @@ var score = 0
 @onready var obstacle_scene = preload("res://obstacle.tscn")
 
 @onready var scoreLabel = $Score;
+@onready var timer = $SpawnTimer;
 var rng = RandomNumberGenerator.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,9 +21,14 @@ func _on_timer_timeout():
 	var obstacle = obstacle_scene.instantiate()
 	obstacle.position = Vector2(150,rng.randf_range(-4, 64))
 	obstacle.passed.connect(_on_obstacle_passed)
+	obstacle.died.connect(_on_obstacle_died)
 	add_child(obstacle)
 
 
 func _on_obstacle_passed():
 	score +=1
+
+func _on_obstacle_died():
+	timer.stop()
+	get_tree().reload_current_scene()
 
--- obstacle.gd
+++ obstacle.gd
@@ -4,16 +4,18 @@ const SPEED = -100
 
 signal passed
 
+signal died
+
 func _process(delta):
 	move_local_x(delta * SPEED)
 
 func _on_visible_on_screen_enabler_2d_screen_exited():
 	queue_free()
 
-
-func _on_area_2d_area_exited():
-	passed.emit()
-
-
 func _on_area_2d_body_exited(body):
 	passed.emit()
+
+
+func _on_area_2d_body_entered(body):
+	died.emit()
+	pass # Replace with function body.
