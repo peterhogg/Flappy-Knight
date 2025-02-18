87d38048d728ba2a345451d7c28db3aed3ba17da
peterhogg <hoggarth.peter@gmail.com>
2025-02-18 02:39:32 +0000

    Deploying to gh-pages from  @ bc67b9ff759224b1804e96b81db7e2fc1ced4720 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1739846132|6001772";
+const CACHE_VERSION = "1739846365|5921677";
 const CACHE_PREFIX = "Flappy Knight-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
