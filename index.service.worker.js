ab999b1d09f4d81cf3a917e5f0744fd45cada3f6
peterhogg <hoggarth.peter@gmail.com>
2025-08-31 17:02:58 +0000

    Deploying to gh-pages from  @ 6632cb66fc3331c9c6027d495215618fc483f3de 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1740099352|4345984';
+const CACHE_VERSION = '1756659766|4293798';
 /** @type {string} */
 const CACHE_PREFIX = 'Flappy Knight-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
